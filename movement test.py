56d3e7ce033820915a6f1dcb52fc0a9d34fb2a4c
josephenewman <josephnewmanyt@gmail.com>
2022-01-27 15:52:49 -0600

    Fixed stalker size and back ground

--- movement test.py
+++ movement test.py
@@ -8,20 +8,20 @@ canvas = tk.Canvas(root, width=800, height=800)
 canvas.pack() # this makes it visible
  
 # Loads and create image (put the image in the folder)
-img = tk.PhotoImage(file="arrow2.gif")
+img = tk.PhotoImage(file="stalker.gif")
 image = canvas.create_image(10, 10, anchor=tk.NW, image=img)
  
  
 def move(event):
     """Move the sprite image with a d w and s when click them"""
     if event.char == "a":
-        canvas.move(image, -10, 0)
+        canvas.move(image, -200, 0)
     elif event.char == "d":
-        canvas.move(image, 10, 0)
+        canvas.move(image, 200, 0)
     elif event.char == "w":
-        canvas.move(image, 0, -10)
+        canvas.move(image, 0, -200)
     elif event.char == "s":
-        canvas.move(image, 0, 10)
+        canvas.move(image, 0, 200)
  
 # This bind window to keys so that move is called when you press a key
 root.bind("<Key>", move)
