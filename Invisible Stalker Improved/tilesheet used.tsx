9bd1f5a8fadb2cc1621ef639f48485bf2bf1242c
josephenewman <josephnewmanyt@gmail.com>
2022-02-09 14:47:17 -0600

    please work

---
+++ Invisible Stalker Improved/tilesheet used.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="tilesheet_complete_2X" tilewidth="128" tileheight="128" tilecount="540" columns="27">
+ <image source="OneDrive/Desktop/mygitfolder/group-2/Invisible Stalker Improved/img/Kenny Graphics/Tilesheet/tilesheet_complete_2X.png" width="3456" height="2560"/>
+</tileset>
